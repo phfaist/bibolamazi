8b98d3576f122494d6141456a8041404dbca9c61
Philippe Faist <pfaist@ethz.ch>
2014-05-12 13:42:45 +0200

    date order mode now also possible

--- filters/orderentries.py
+++ filters/orderentries.py
@@ -3,11 +3,14 @@ import os
 import os.path
 import re
 import codecs
+import datetime
+import calendar
 
 from pybtex.database import BibliographyData;
 
 
 from core import bibfilter
+from core import butils
 from core.bibfilter import BibFilter, BibFilterError;
 from core.blogger import logger;
 
@@ -31,19 +34,31 @@ Possible ordering modes are:
   - 'alphabetical'
     Orders all the entries in the bibtex file alphabetically according to the citation key.
 
+  - 'date'
+    Orders all the entries in the bibtex file by publication date, most recent first.
+
+
+Note the '-dReverse' option which flips the ordering for 'alphabetical' or 'date' ordering
+mode. This option has no effect for 'raw' mode.
 
 This is particularly useful for keeping the bibtex file in a VCS (git, svn etc.)
 and getting relevant diffs between different revisions.
 """
 
 
+
+
+
 # modes
 ORDER_RAW = 0
 ORDER_CITATION_KEY_ALPHA = 1
+ORDER_DATE = 2
 
 
 OrderMode = bibfilter.enum_class('OrderMode', [('raw', ORDER_RAW),
-                                               ('alphabetical', ORDER_CITATION_KEY_ALPHA)],
+                                               ('alphabetical', ORDER_CITATION_KEY_ALPHA),
+                                               ('date', ORDER_DATE),
+                                               ],
                                  default_value='alphabetical',
                                  value_attr_name='ordermode')
 
@@ -56,15 +71,17 @@ class OrderEntriesFilter(BibFilter):
     helptext = HELP_TEXT
     
     
-    def __init__(self, order=None):
+    def __init__(self, order=None, reverse=False):
         """
         Arguments:
           - order(OrderMode): The strategy according to which to order all the entries. Possible
                 values: see below.
+          - reverse(bool): Reverse the sorting order. Has no effect with 'raw' order mode.
         """
         BibFilter.__init__(self);
 
         self.order = OrderMode(order)
+        self.reverse = butils.getbool(reverse)
 
         logger.debug('orderentries: self.order=%r' % self.order);
 
@@ -94,10 +111,52 @@ class OrderEntriesFilter(BibFilter):
             # bibdata.entries is of type pybtex.util.OrderedCaseInsensitiveDict, which has
             # an attribute `order`, which is a list of keys in the relevant order. So use
             # list.sort(), which is slightly more efficient.
-            bibdata.entries.order.sort()
+            bibdata.entries.order.sort(reverse=self.reverse)
 
             #newbibdata = BibliographyData(entries=newentries);
             #bibolamazifile.setBibliographyData(newbibdata);
+
+        elif (self.order == ORDER_DATE):
+
+            bibdata = bibolamazifile.bibliographydata();
+
+            entries = bibdata.entries;
+
+            def getpubdate(key):
+                entry = entries.get(key)
+                if entry is None:
+                    return datetime.today()
+                fields = entry.fields
+                year = datetime.MAXYEAR
+                month = 12
+                if 'year' in fields:
+                    try:
+                        year = int(fields['year'])
+                    except ValueError:
+                        pass
+
+                if 'month' in fields:
+                    mon_s = re.sub('[^a-z]', '', fields['month'].lower())
+                    month = next( (1+k for k in range(len(_month_regexps))
+                                   if (_month_regexps[k].match(mon_s)) ), 12 )
+                    logger.debug('\t%s: mon_s=%s; month=%d', key, mon_s, month);
+
+                day = calendar.monthrange(year, month)[1];
+                if 'day' in fields:
+                    try:
+                        day = int(fields['day'])
+                    except ValueError:
+                        pass
+
+                try:
+                    return datetime.date(year, month, day)
+                except ValueError as e:
+                    logger.warning("Can't parse date for entry %s: %s", key, e)
+                    return datetime.date.today()
+
+            # see above. Note the "not reverse" because the date key will sort
+            # increasingly, whereas we want the default sort order to be newest first.
+            bibdata.entries.order.sort(key=getpubdate, reverse=(not self.reverse));
 
         elif (self.order == ORDER_RAW):
             # natural order mode. don't do anything.
@@ -108,6 +167,23 @@ class OrderEntriesFilter(BibFilter):
         logger.debug("ordered entries as wished.");
 
         return
+
+
+_month_regexps = (
+    re.compile('^jan'),
+    re.compile('^feb'),
+    re.compile('^mar'),
+    re.compile('^apr'),
+    re.compile('^may'),
+    re.compile('^jun'),
+    re.compile('^jul'),
+    re.compile('^aug'),
+    re.compile('^sep'),
+    re.compile('^oct'),
+    re.compile('^nov'),
+    re.compile('^dec'),
+    )
+
 
 
 def bibolamazi_filter_class():
