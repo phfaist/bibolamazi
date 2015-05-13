051f26e25faad2ef9423406d7cdbe2650fbd97f9
Philippe Faist <pfaist@ethz.ch>
2015-05-13 10:31:29 +0200

    doc update

--- doc/develfilterexample.py
+++ doc/develfilterexample.py
@@ -27,10 +27,10 @@ HELP_TEXT = u"""
 There are three possible operating modes:
 
     "empty"  -- add an empty field 'testField' to all entries.
-    "random" -- the content of the 'testField' field which we add to all entries is
-                completely random.
-    "fixed"  -- the content of the 'testField' field which we add to all entries is
-                a hard-coded, fixed string. Surprise!
+    "random" -- the content of the 'testField' field which we add to all entries
+                is completely random.
+    "fixed"  -- the content of the 'testField' field which we add to all entries
+                is a hard-coded, fixed string. Surprise!
 
 Specify which operating mode you prefer with the option '-sMode=...'. By default,
 "random" mode is assumed.
@@ -38,11 +38,11 @@ Specify which operating mode you prefer with the option '-sMode=...'. By default
 
 # --- operating modes ---
 
-# Here we define a custom enumeration type for passing as argument to our constructor. By
-# doing it this way, instead of simply accepting a string, allows the filter factory
-# mechanism to help us report errors and provide more helpful help messages. Also, in the
-# graphical interface the relevant option is presented as a drop-down list instead of a
-# text field.
+# Here we define a custom enumeration type for passing as argument to our
+# constructor. By doing it this way, instead of simply accepting a string,
+# allows the filter factory mechanism to help us report errors and provide more
+# helpful help messages. Also, in the graphical interface the relevant option is
+# presented as a drop-down list instead of a text field.
 
 # numerical values -- numerical values just have to be different
 MODE_EMPTY = 0
@@ -57,7 +57,8 @@ _modes = [
     ]
 
 # our Mode type. See `bibolamazi.core.bibfilter.argtypes`
-Mode = enum_class('Mode', _modes, default_value=MODE_NONE, value_attr_name='mode')
+Mode = enum_class('Mode', _modes, default_value=MODE_NONE,
+                  value_attr_name='mode')
 
 
 # --- the filter object itself ---
@@ -71,11 +72,16 @@ class MyTestFilter(BibFilter):
 
     def __init__(self, mode="random"):
         """
-        Constructor method for TestFilter. Note that this part of the constructor
-        docstring itself isn't that useful, but the argument list below is parsed and used
-        by the default automatic option parser for filter arguments. So document your
-        arguments! If your filter accepts **kwargs, you may add more arguments below than
-        you explicitly declare in your constructor prototype.
+        Constructor method for TestFilter. Note that this part of the
+        constructor docstring itself isn't that useful, but the argument list
+        below is parsed and used by the default automatic option parser for
+        filter arguments. So document your arguments! If your filter accepts
+        `**kwargs`, you may add more arguments below than you explicitly declare
+        in your constructor prototype.
+
+        If this function accepts `*args`, then additional positional arguments
+        on the filter line will be passed to those args. (And not to the
+        declared arguments.)
 
         Arguments:
           - mode(Mode): the operating mode to adopt
@@ -89,16 +95,18 @@ class MyTestFilter(BibFilter):
 
     def action(self):
         # Here, we want the filter to operate entry-by-entry (so the function
-        # `self.filter_bibentry()` will be called). If we had preferred to operate on the
-        # whole bibliography database in one go (as, e.g., for the `duplicates` filter),
-        # then we would have to return `BibFilter.BIB_FILTER_BIBOLAMAZIFILE` here, and
-        # provide a `filter_bibolamazifile()` method.
+        # `self.filter_bibentry()` will be called). If we had preferred to
+        # operate on the whole bibliography database in one go (as, e.g., for
+        # the `duplicates` filter), then we would have to return
+        # `BibFilter.BIB_FILTER_BIBOLAMAZIFILE` here, and provide a
+        # `filter_bibolamazifile()` method.
         #
         return BibFilter.BIB_FILTER_SINGLE_ENTRY
 
     def requested_cache_accessors(self):
-        # return the requested cache accessors here if you are using the cache mechanism. 
-        # This also applies if you are using the `arxivutil` utilities.
+        # return the requested cache accessors here if you are using the cache
+        # mechanism.  This also applies if you are using the `arxivutil`
+        # utilities.
         return [ ]
 
     def filter_bibentry(self, entry):
@@ -116,8 +124,9 @@ class MyTestFilter(BibFilter):
 
         if self.mode == MODE_FIXED:
             entry.fields['testField'] = (
-                u"On d\u00E9daigne volontiers un but qu'on n'a pas r\u00E9ussi "
-                u"\u00E0 atteindre, ou qu'on a atteint d\u00E9finitivement. (Proust)"
+                u"On d\u00E9daigne volontiers un but qu'on n'a pas "
+                u"r\u00E9ussi \u00E0 atteindre, ou qu'on a atteint "
+                u"d\u00E9finitivement. (Proust)"
                 )
         
         raise BibFilterError('testfilter', "Unknown operating mode: %s" % mode )
@@ -125,8 +134,9 @@ class MyTestFilter(BibFilter):
 
 
 #
-# Every python module which defines a filter should have the following method, which
-# returns the filter class type (which is expected to be a `BibFilter` subclass).
+# Every python module which defines a filter should have the following method,
+# which returns the filter class type (which is expected to be a `BibFilter`
+# subclass).
 #
 def bibolamazi_filter_class():
     return MyTestFilter
