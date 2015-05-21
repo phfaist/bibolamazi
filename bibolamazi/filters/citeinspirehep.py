66f16ce55ce732cc616fe2b48b7986ef50e37a08
Romain Mueller <romain.mueller@gmail.com>
2015-05-21 19:07:11 +0200

    better regexp for allowed chars

--- bibolamazi/filters/citeinspirehep.py
+++ bibolamazi/filters/citeinspirehep.py
@@ -99,7 +99,8 @@ class InspireHEPFetchedAPIInfoCacheAccessor(BibUserCacheAccessor):
 
         if key.startswith('inspire:'):
             key = key[len('inspire:'):].strip()
-            allowedchars = r'A-Za-z0-9_.-\\\''
+            allowedchars = r"A-Za-z0-9_.'-"
+            print "helo"
             if re.match(r'[^'+allowedchars+r']', key):
                 # Report error rather than removing the spaces and special characters,
                 # otherwise the user might spend hours figuring out why "PhysRev 47 777"
