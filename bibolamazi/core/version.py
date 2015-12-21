810ff23087833fa5030f5455b157f2df5efd4d2f
Philippe Faist <pfaist@ethz.ch>
2015-12-21 13:06:19 +0100

    bumped version number to "3.2.post1" to fix a pypi upload

--- bibolamazi/core/version.py
+++ bibolamazi/core/version.py
@@ -23,7 +23,7 @@
 # This is the current BIBOLAMAZI version.
 # Bump the number here for different version numbers.
 
-version_str = "3.2"
+version_str = "3.2.post1"
 """
 The version string. This is increased upon each release.
 """
