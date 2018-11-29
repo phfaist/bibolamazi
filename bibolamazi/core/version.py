68a03ce9f1e35f9d3949bea155830d1351624e0f
Philippe Faist <phfaist@gmail.com>
2018-11-28 17:05:00 -0800

    beta version b4 bump

--- bibolamazi/core/version.py
+++ bibolamazi/core/version.py
@@ -23,7 +23,7 @@
 # This is the current BIBOLAMAZI version.
 # Bump the number here for different version numbers.
 
-version_str = "4.0b3"
+version_str = "4.0b4"
 """
 The version string. This is increased upon each release.
 """
